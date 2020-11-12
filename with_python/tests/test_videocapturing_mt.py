afe5e27a687937e50f1c5fcae10c292f08d40b85
Mengqi.Ye <m18916103034@163.com>
2020-11-12 12:40:00 +0800

    Add other multithreading examples.

--- with_python/tests/test_videocapturing_mt.py
+++ with_python/tests/test_videocapturing_mt.py
@@ -1,33 +1,47 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created by Mengqi Ye on 2020/11/3
+Created by Mengqi Ye on 2020/11/4
 """
-import random
-from threading import Thread
-
-import stl
+# Imports
 import sys
-import time
 
 import cv2
 import time
 import threading
 
-print(dir(stl))
+# Camera settings go here
+imageWidth = ...
+imageHeight = ...
+frameRate = ...
+processingThreads = 4
 
 # Shared values
 global running
+global cap
 global frameLock
 global processorPool
 running = True
+
+# Setup the camera
+cap = cv2.VideoCapture(0)
+imageWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+imageHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frameRate = int(cap.get(cv2.CAP_PROP_FPS))
+length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
 frameLock = threading.Lock()
+
+print(f"imageWidth : {imageWidth}, imageHeight : {imageHeight}, frameRate : {frameRate}, length : {length}")
+
+if not cap.isOpened():
+    cap.open(0)
 
 
 # Image processing thread, self-starting
-class NormalThread(threading.Thread):
+class ImageProcessor(threading.Thread):
     def __init__(self, name, autoRun=True):
-        super(NormalThread, self).__init__()
+        super(ImageProcessor, self).__init__()
         self.event = threading.Event()
         # self.eventWait = (2.0 * processingThreads) / frameRate
         self.eventWait = .1
@@ -41,40 +55,39 @@ class NormalThread(threading.Thread):
         global frameLock
         global processorPool
         while running:
-            print(f"native_id : %10d, ident : %20d\n" % (self.native_id, self.ident))
-            # time.sleep(10)
             # Wait for an image to be written to the stream
             self.event.wait(self.eventWait)
             if self.event.isSet():
                 if not running:
                     break
                 try:
-                    self.gen_normal_distribution(self.next_param)
+                    self.ProcessImage(self.nextFrame)
                 finally:
                     # Reset the event
-                    self.next_param = None
+                    self.nextFrame = None
                     self.event.clear()
                     # Return ourselves to the pool at the back
                     with frameLock:
                         processorPool.insert(0, self)
         print('Processor thread %s terminated' % (self.name))
 
-    def gen_normal_distribution(self, next_param):
+    def ProcessImage(self, image):
         # Processing for each image goes here
-        mu, std = next_param
-        a = stl.Chromosome([])
-        stl.normal(a, 100, mu, std)
-        print(a)
+        ### TODO ###
+        print(type(image))
 
 
-class ParamThread(threading.Thread):
+# Image capture thread, self-starting
+class ImageCapture(threading.Thread):
     def __init__(self):
-        super(ParamThread, self).__init__()
+        super(ImageCapture, self).__init__()
         self.start()
 
+    # Stream delegation loop
     def run(self):
         # This method runs in a separate thread
         global running
+        global cap
         global processorPool
         global frameLock
         while running:
@@ -86,28 +99,24 @@ class ParamThread(threading.Thread):
                     processor = None
             if processor:
                 # Grab the next frame and send it to the processor
-                epsilon = 1e-6
-                rand_mean = random.random()
-                rand_std = min(min(random.random(), rand_mean - epsilon), 1 - rand_mean - epsilon) / 3
-
-                processor.next_param = (rand_mean, rand_std)
-                processor.event.set()
+                success, frame = cap.read()
+                if success:
+                    processor.nextFrame = frame
+                    processor.event.set()
+                else:
+                    print('Capture stream lost...')
+                    running = False
             else:
                 # When the pool is starved we wait a while to allow a processor to finish
                 time.sleep(0.01)
         print('Capture thread terminated')
 
 
-def main_mp(x):
-    a = stl.Chromosome([])
-    stl.normal(a, 10, 0.5, 0.1)
-    print(a)
-
-
 # Create some threads for processing and frame grabbing
-processorPool = [NormalThread(i + 1) for i in range(4)]
+processorPool = [ImageProcessor(i + 1) for i in range(processingThreads)]
 allProcessors = processorPool[:]
-paramThread = ParamThread()
+captureThread = ImageCapture()
+
 # Main loop, basically waits until you press CTRL+C
 # The captureThread gets the frames and passes them to an unused processing thread
 try:
@@ -133,5 +142,7 @@ while allProcessors:
     processor.join()
 
 # Cleanup the capture thread
-paramThread.join()
+captureThread.join()
 
+# Cleanup the camera object
+cap.release()
